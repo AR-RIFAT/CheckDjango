2451536adfd9ba55c64b4011052d7f59f8e62e12
rowan1697 <rowanhossain@gmail.com>
2018-07-10 01:27:10 +0600

    Rowan's commit

--- music/views.py
+++ music/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-     return HttpResponse("<h2> Checking .... !! </h2>")
+     return HttpResponse("<h2> Checking .... .........!! </h2>")
 
 
 def details(request, album_id):
